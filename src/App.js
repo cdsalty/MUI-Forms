e076e8476a4f34c02c049faea4ddcd805470b24b
cdsalty <soltiscd@gmail.com>
2021-08-27 01:18:24 -0400

    fixed bug; fisnished TextField Lesson

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Textfield from "./Components/FormsUI/Textfield";
 // import DateTimePicker from "./Components/FormsUI/DataTimePicker";
 // import Checkbox from "./Components/FormsUI/Checkbox";
 // import Button from "./Components/FormsUI/Button";
-// import countries from "./data/countries.json";
+import countries from "./data/countries.json";
 
 const useStyles = makeStyles(theme => ({
   formWrapper: {
@@ -18,15 +18,17 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+// Initial Form State (All must match the name parameter exactly) -------------------------------------------------------
+
 const INITIAL_FORM_STATE = {
   firstName: "",
   lastName: "",
   email: "",
-  phone: ""
-  // addressLine1: "",
-  // addressLine2: "",
-  // city: "",
-  // state: "",
+  phone: "",
+  addressLine1: "",
+  addressLine2: "",
+  city: "",
+  state: ""
   // country: "",
   // arrivealDate: "",
   // departureDate: "",
@@ -34,43 +36,104 @@ const INITIAL_FORM_STATE = {
   // termsOfService: false
 };
 
-// YUP Validation
+// Yup! ----------------------------------------------------------------------------------------------------------
+
 const FORM_VALIDATION = Yup.object().shape({
-  firstName: Yup.string().required("Required to enter"),
-  lastName: Yup.string().required("Required to enter last name"),
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
   email: Yup.string()
-    .email("Invalid Email")
-    .required("You ain't special Mr."),
+    .email("Invalid email.")
+    .required("Required"),
   phone: Yup.number()
     .integer()
-    .typeError("Enter valid number")
-    .required("Must include a number")
+    .typeError("Please enter a valid phone number")
+    .required("Required"),
+  addressLine1: Yup.string().required("Required"),
+  addressLine2: Yup.string(),
+  city: Yup.string().required("Required"),
+  state: Yup.string().required("Required")
+  // country: Yup.string().required("Required"),
+  // arrivealDate: Yup.date().required("Required"),
+  // departureDate: Yup.date().required("Required"),
+  // message: Yup.string(),
+  // termsOfService: Yup.boolean()
+  //   .oneOf([true], "The terms and conditions must be accepted.")
+  //   .required("The terms and conditions must be accepted.")
 });
+
+// APP.JS -----------------------------------------------------------------------------------------------------
 
 const App = () => {
   const classes = useStyles();
 
   return (
-    <Form>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography>Your details</Typography>
-        </Grid>
-
-        <Grid item xs={6}>
-          {/* Important... name must match the inital "firstName" value that was inside of initialValues */}
-          <Textfield name="firstName" lable="First Name" />
-        </Grid>
-        <Grid item xs={6}>
-          <Textfield
-            name="lastName"
-            // since we brought in ...otherProps, we can pass in any other material ui components we want to pass in. Such as label
-
-            label="Last Name"
-          />
-        </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
       </Grid>
-    </Form>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            {/*Formik Start ----------------------------------------------------------------------------------------------------- */}
+            <Formik
+              initialValues={{
+                ...INITIAL_FORM_STATE
+              }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={values => {
+                console.log(values);
+              }}
+            >
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography>Your details</Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="firstName" label="First Name" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="lastName" label="Last Name" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="email" label="Email" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="phone" label="Phone" />
+                  </Grid>
+
+                  {/* Look up to center */}
+                  <Grid item xs={12}>
+                    <Typography>Address</Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield name="addressLine1" label="Address Line 1" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield name="addressLine2" label="Address Line 2" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="city" label="City" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="state" label="State" />
+                  </Grid>
+                  {/* Next Section ---------------------------------------------------------------------------------------------- */}
+                </Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/Components/FormsUI/Textfield/index.js
+++ src/Components/FormsUI/Textfield/index.js
@@ -29,3 +29,38 @@ const TextfieldWrapper = ({ name, ...otherProps }) => {
 };
 
 export default TextfieldWrapper;
+// https://youtu.be/MV9NC3FoCmM?t=889
+
+/*
+
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import { useField } from 'formik';
+
+const TextfieldWrapper = ({
+  name,
+  ...otherProps
+}) => {
+  const [field, mata] = useField(name);
+
+  const configTextfield = {
+    ...field,
+    ...otherProps,
+    fullWidth: true,
+    variant: 'outlined'
+  };
+
+  if (mata && mata.touched && mata.error) {
+    configTextfield.error = true;
+    configTextfield.helperText = mata.error;
+  }
+
+  return (
+    <TextField {...configTextfield} />
+  );
+};
+
+export default TextfieldWrapper;
+
+
+*/
